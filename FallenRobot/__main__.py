1e7dd0b45b01448415608429b71fc7b574f5c24f
ᴛʜᴇ ᴀɴᴏɴʏᴍᴏᴜs <TheAnonymous2510@gmail.com>
2022-11-30 16:58:38 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -31,10 +31,8 @@ import FallenRobot.modules.sql.users_sql as sql
 from FallenRobot import (
     BOT_NAME,
     BOT_USERNAME,
-    CERT_PATH,
     LOGGER,
     OWNER_ID,
-    PORT,
     START_IMG,
     SUPPORT_CHAT,
     TOKEN,
@@ -774,18 +772,9 @@ def main():
 
     dispatcher.add_error_handler(error_callback)
 
-    if WEBHOOK:
-        LOGGER.info("Using webhooks.")
-        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 
-        if CERT_PATH:
-            updater.bot.set_webhook(url=URL + TOKEN, certificate=open(CERT_PATH, "rb"))
-        else:
-            updater.bot.set_webhook(url=URL + TOKEN)
-
-    else:
-        LOGGER.info("Using long polling.")
-        updater.start_polling(timeout=15, read_latency=4, clean=True)
+    LOGGER.info("Using long polling.")
+    updater.start_polling(timeout=15, read_latency=4, clean=True)
 
     if len(argv) not in (1, 3, 4):
         telethn.disconnect()

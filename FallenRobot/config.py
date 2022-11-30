afa563d33564f8e5f86c30119393284e18f630c5
ᴛʜᴇ ᴀɴᴏɴʏᴍᴏᴜs <TheAnonymous2510@gmail.com>
2022-11-30 18:07:27 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -1,56 +1,57 @@
 class Config(object):
     LOGGER = True
 
+  # Get this value from my.telegram.org/apps
     API_ID = 
     API_HASH = ""
-    TOKEN = ""
-    OWNER_ID = 
-    OWNER_USERNAME = ""
-    SUPPORT_CHAT = ""
-    JOIN_LOGGER = ()
+
+  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
+    CASH_API_KEY = ""
+
+  # A sql database url from elephantsql.com
+    DATABASE_URL = ""
+
+  # Event logs channel to note down important bot level events
     EVENT_LOGS = ()
 
-    SQLALCHEMY_DATABASE_URI = ""
+  # Get ths value from cloud.mongodb.com
     MONGO_DB_URI = ""
-    LOAD = []
-    NO_LOAD = ["rss"]
-    WEBHOOK = False
-    INFOPIC = True
-    URL = None
 
-    ## Optional
-    DRAGONS = []
-    DEV_USERS = []
-    DEMONS = []
-    TIGERS = []
-    WOLVES = []
-
-    CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = True
-    STRICT_GBAN = True
-    WORKERS = (8)
-    BAN_STICKER = (
-        "CAACAgUAAxkBAAEDafNhq5Z0DegqVzauwSighMw5cPWp8QACVgQAAuUG0FRXfCEuBziNzCIE"
-    )
-    ALLOW_EXCL = True
-    CASH_API_KEY = ""
-    TIME_API_KEY = ""
-    BL_CHATS = []
-    SPAMMERS = None
-    ALLOW_CHATS = True
+  # Telegraph link of the image which will be shown at start command.
     START_IMG = ""
-    HEROKU_API_KEY = None
-    HEROKU_APP_NAME = None
-    TEMP_DOWNLOAD_DIRECTORY = "./"
-    ALLOW_EXCL = None
-    BL_CHATS = []  # List of groups that you want blacklisted.
-    SPAMMERS = None
 
+  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = ""
+
+  # Get bot token from @BotFather on Telegram
+    TOKEN = ""
+
+  # Get this value from https://timezonedb.com/api
+    TIME_API_KEY = ""
+
+  # User id of your telegram account (Must be integer)
+    OWNER_ID = 
+
+  # Optional fields
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    DRAGONS = []  # User id of sudo users
+    DEV_USERS = []  # User id of dev users
+    DEMONS = []  # User id of support users
+    TIGERS = []  # User id of tiger users
+    WOLVES = []  # User id of whitelist users
+
+    ALLOW_CHATS = True
+    ALLOW_EXCL = True
+    DEL_CMDS = True
+    INFOPIC = True
+    LOAD = []
+    NO_LOAD = []
+    STRICT_GBAN = True
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    WORKERS = (8)
 
 class Production(Config):
     LOGGER = True
-
 
 class Development(Config):
     LOGGER = True
